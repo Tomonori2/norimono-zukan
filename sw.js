2a506415b9b8d01c8203938b753df60525e70e9d
Tomonori2 <tomonori2@users.noreply.github.com>
2026-04-12 13:56:25 +0900

    のりもの図鑑クイズアプリ

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE = 'kuruma-quiz-v3';
+const FILES = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+// ネットワーク優先: 常に最新を取りに行き、オフライン時のみキャッシュを使う
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(()=>{});
+        return res;
+      })
+      .catch(() => caches.match(e.request))
+  );
+});
